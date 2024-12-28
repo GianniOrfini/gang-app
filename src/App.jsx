4d2bedccfaad5495cabae73bcbc1d1e800cd9761
Gianni <gianni@MacBook-Air-de-Gianni.local>
2024-12-28 17:46:52 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>GangApp project</h1>
+      <img src="./gif.gif" />
+    </>
+  )
+}
+
+export default App
